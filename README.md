7423392e83461d62be2ca199c89bbf327e769c64
yngve <yngve@skvupp.com>
2022-06-12 10:04:36 +0200

    Create README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Getting Started with podcastServer
+## Setup
+Create account on https://auth0.com/ Create an API. Create a .env file in the project root and fill in data:
+```
+AUTH0_DOMAIN=[accountname].eu.auth0.com
+CLIENT_ID="[Client id of API]"
+CLIENT_SECRET="[Client secret of API]"
+AUDIENCE="[Audience of API]"
+```
+
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,6 @@ import {auth} from 'express-oauth2-jwt-bearer';
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 export const checkJwt = auth({
-    audience: 'https://itkode.no/podcastAPI',
-    issuerBaseURL: `https://itkode.eu.auth0.com/`,
+    audience: `${process.env.AUDIENCE}`,
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
 });
